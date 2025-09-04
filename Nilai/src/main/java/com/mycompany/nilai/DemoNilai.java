68e4574024b6441b1279336c5eb825f479a0c7e3
DiegoAndreasSimanjuntak <diegosimanjuntak2006@gmail.com>
2025-09-04 15:45:01 +0700

    Add files via upload

---
+++ Nilai/src/main/java/com/mycompany/nilai/DemoNilai.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.nilai;
+
+/**
+ *
+ * @author Diego
+ */
+public class DemoNilai {
+    public static void main(String[] args) {
+        Nilai nilaiMhs = new Nilai("2410631170068", "Diego", 90, 85, 80, 90);
+
+        nilaiMhs.CetakNilai();
+    }
+}
---
+++ Nilai/src/main/java/com/mycompany/nilai/Nilai.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nilai;
+
+/**
+ *
+ * @author Diego
+ */
+public class Nilai {
+    String nim;
+    String nama;
+    double absen;
+    double tugas;
+    double uts;
+    double uas;
+    double nilaiAkhir;
+
+    public Nilai(String nim, String nama, double absen, double tugas, double uts, double uas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.absen = absen;
+        this.tugas = tugas;
+        this.uts = uts;
+        this.uas = uas;
+        this.nilaiAkhir = (0.1 * absen) + (0.2 * tugas) + (0.3 * uts) + (0.4 * uas);
+    }
+
+    void CetakNilai() {
+        System.out.println("NIM : " + nim);
+        System.out.println("Nama : " + nama);
+        System.out.println("Nilai Absen [10%] : " + absen);
+        System.out.println("Nilai Tugas [20%] : " + tugas);
+        System.out.println("Nilai UTS [30%] : " + uts);
+        System.out.println("Nilai UAS [40%] : " + uas);
+        System.out.println("Nilai Akhir : " + nilaiAkhir);
+    }
+}
